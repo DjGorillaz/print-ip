82cdbee4d271739850ce4fbca366c236319003db
Unknown <djg2008@gmail.com>
2018-03-27 13:54:58 +0300

    Template specialization for string

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <type_traits>
-
 #include <string>
 #include <vector>
 #include <list>
 #include <tuple>
+
+//String
+template <typename T>
+typename std::enable_if<std::is_same<std::string, T>::value>::type printIp(T&& v)
+{
+    std::cout << v << std::endl;
+}
 
 //Integral numbers
 template <typename T>
@@ -21,13 +27,9 @@ typename std::enable_if<std::is_integral<T>::value>::type printIp(T&& v)
     std::cout << std::endl;
 }
 
-
 //Containers
 template <typename T>
 struct is_container: std::false_type { };
-
-template <>
-struct is_container<std::string>: std::true_type { };
 
 template <typename T>
 struct is_container<std::list<T>>: std::true_type { };
@@ -36,13 +38,13 @@ template <typename T>
 struct is_container<std::vector<T>>: std::true_type { };
 
 template <typename T>
-typename std::enable_if<is_container<T>::value>::type printIp(T& t)
+typename std::enable_if<is_container<T>::value>::type printIp(T&& t)
 {
     std::string d = "";
     for(const auto& val: t)
     {
         std::cout << d << val;
-        d = std::is_same<T, std::string>::value ? "" : "..";
+        d = "..";
     }
     std::cout << std::endl;
 }
@@ -62,7 +64,7 @@ template <typename T, typename ... Args>
 struct is_same_tuple<std::tuple<T, T, Args...>>: is_same_tuple<std::tuple<T, Args...>> { };
 
 template <typename T>
-typename std::enable_if<is_same_tuple<T>::value>::type printIp(const T& t)
+typename std::enable_if<is_same_tuple<T>::value>::type printIp(T&& t)
 {
     constexpr std::size_t size = std::tuple_size<T>::value;
     printTuple<0, size-1, T>{}(t);
@@ -98,11 +100,11 @@ int main()
     printIp(char(-1));
     printIp(short(0));
     printIp(int(2130706433));
-    printIp(long(8875824491850138409));
-    printIp(str);
-    printIp(vec);
-    printIp(list);
-    printIp(tup);
+    printIp(long long(8875824491850138409));
+    printIp(std::move(str));
+    printIp(std::move(vec));
+    printIp(std::move(list));
+    printIp(std::move(tup));
 
     return 0;
 }
